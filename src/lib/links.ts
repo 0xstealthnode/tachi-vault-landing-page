30bb0548927825e9c80723715c5a32808af8eaf5
0xstealthnode <0xstealthnode@gmail.com>
2026-04-28 05:46:48 +0000

    fix: update download links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,6 +1,6 @@
 export const CHROME_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1T_dQbS2KhQ9T75hXtAaAakFULsz5APa-'
+  'https://drive.google.com/uc?export=download&id=19Cg9b-sOFt_TSxEKIhqOrAqZ5PwWDVV4'
 export const FIREFOX_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1laJot6k4l0PP7loQfCxcW02oCqkRdNj2'
+  'https://drive.google.com/uc?export=download&id=1Yz0LCSyeNZ1b1wwG0n7oy10t41LSWnSI'
 
 export const GITHUB_URL = 'https://github.com/' // TODO: set repo URL
