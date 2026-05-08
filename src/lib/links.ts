4eab41479812126f246596b5d16800d41fb98acd
0xstealthnode <0xstealthnode@gmail.com>
2026-05-08 10:53:08 +0000

    chore: update download links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,6 +1,6 @@
 export const CHROME_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1SUxfwhoSFoBIk8l7dq5mKMslKTX_NVcT'
+  'https://drive.google.com/uc?export=download&id=1on8STouTUTXyVFZiXYXTAGyeM_C4LG6d'
 export const FIREFOX_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1W1rIf-WfKnEpj0KXnE7CT69NIehH5Hfh'
+  'https://drive.google.com/uc?export=download&id=1C5u4kdp_c0cQk1keboBtWJAr4IDmeT5s'
 
-export const GITHUB_URL = 'https://github.com/' // TODO: set repo URL
+export const GITHUB_URL = 'https://github.com/'
