65948997352b3b645c1ceef79934b8de1a2de013
0xstealthnode <0xstealthnode@gmail.com>
2026-07-20 07:48:02 +0000

    chore: bump version to 1.0.1 and update download links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,6 +1,6 @@
 export const CHROME_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1MEBA9f-aeNFFbKZLaurK-uoEBfBX_GGz'
+  'https://drive.google.com/uc?export=download&id=1bPfuCWI_Awj_zFFZqHJ1mx7v9ui7ZJnb'
 export const FIREFOX_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1DM6W8llrj0ahJ6WvsKQCQ3CQcuI0Yf24'
+  'https://drive.google.com/uc?export=download&id=1TtUR8RSss1LvomMZslNGRU-MnFglgb03'
 
 export const GITHUB_URL = 'https://github.com/'
