36171ceeea0d6ae0b51a7ea513ca0e6a7a3b926c
0xstealthnode <0xstealthnode@gmail.com>
2026-07-28 10:04:17 +0000

    chore: update download links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,6 +1,6 @@
 export const CHROME_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1bPfuCWI_Awj_zFFZqHJ1mx7v9ui7ZJnb'
+  'https://drive.google.com/uc?export=download&id=1_3oOgRCzYzlu7U-SNhGRgBSvULqtUo-c'
 export const FIREFOX_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1TtUR8RSss1LvomMZslNGRU-MnFglgb03'
+  'https://drive.google.com/uc?export=download&id=1_4bHpMoJ5WsOB9l4VBNNCDR2Y7CsOoQZ'
 
 export const GITHUB_URL = 'https://github.com/'
