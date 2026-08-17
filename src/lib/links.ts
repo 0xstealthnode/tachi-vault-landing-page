27b7d2f69aaf87102eb54dca26ca339e72b80d0f
0xstealthnode <0xstealthnode@gmail.com>
2026-08-17 12:40:09 +0000

    update links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,6 +1,6 @@
 export const CHROME_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1_3oOgRCzYzlu7U-SNhGRgBSvULqtUo-c'
+  'https://drive.google.com/uc?export=download&id=1_9kQkMIre-UN8dt8zsCxoe8gkM3rxucF'
 export const FIREFOX_ZIP_URL =
-  'https://drive.google.com/uc?export=download&id=1_4bHpMoJ5WsOB9l4VBNNCDR2Y7CsOoQZ'
+  'https://drive.google.com/uc?export=download&id=1YzYM1k_5_rPmJCS2jCK2lofSQ9vT1dpA'
 
 export const GITHUB_URL = 'https://github.com/'
